d0dfe107c30bcbceeed1336335ec133a129a424d
illia.herman <illiaherman8@gmail.com>
2022-02-05 14:12:18 +0100

    init api service

--- app/src/main/java/com/example/android/politicalpreparedness/util/Const.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/util/Const.kt
@@ -1,7 +1,17 @@
 package com.example.android.politicalpreparedness.util
 
+import com.example.android.politicalpreparedness.BuildConfig
+
 object Const {
     // Database
     const val ELECTION_DATABASE_NAME = "election_database"
     const val ELECTION_TABLE_NAME = "election_table"
+
+    //API
+    const val apiKey = BuildConfig.API_KEY
+    const val keyQueryParameter = "key"
+    const val BASE_URL = "https://www.googleapis.com/civicinfo/v2/"
+    const val GET_ELECTIONS = "elections"
+    const val GET_VOTERINFO = "voterinfo"
+    const val GET_REPRESENTATIVES = "representatives"
 }
